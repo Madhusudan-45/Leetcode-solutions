a8cab7ee4531a2af0592ba436c62d682244ffcc4
Madhusudan-45 <madhusudan.451813@gmail.com>
2026-08-10 21:29:51 +0530

    Time: 0 ms (100%), Space: 20.9 MB (57.04%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,46 +1,102 @@
+// class Solution {
+// public:
+//     void setZeroes(vector<vector<int>>& matrix) {
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+
+//         bool firstRow = false;
+//         bool firstCol = false;
+
+//         for (int j = 0; j < n; j++) {
+//             if (matrix[0][j] == 0)
+//                 firstRow = true;
+//         }
+
+//         for (int i = 0; i < m; i++) {
+//             if (matrix[i][0] == 0)
+//                 firstCol = true;
+//         }
+
+//         for (int i = 1; i < m; i++) {
+//             for (int j = 1; j < n; j++) {
+//                 if (matrix[i][j] == 0) {
+//                     matrix[i][0] = 0;
+//                     matrix[0][j] = 0;
+//                 }
+//             }
+//         }
+
+//         for (int i = 1; i < m; i++) {
+//             for (int j = 1; j < n; j++) {
+//                 if (matrix[i][0] == 0 || matrix[0][j] == 0)
+//                     matrix[i][j] = 0;
+//             }
+//         }
+
+//         if (firstRow) {
+//             for (int j = 0; j < n; j++)
+//                 matrix[0][j] = 0;
+//         }
+
+//         if (firstCol) {
+//             for (int i = 0; i < m; i++)
+//                 matrix[i][0] = 0;
+//         }
+//     }
+// };
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
 
-        bool firstRow = false;
-        bool firstCol = false;
+        int n = matrix.size();
+        int m = matrix[0].size();
 
-        for (int j = 0; j < n; j++) {
-            if (matrix[0][j] == 0)
-                firstRow = true;
-        }
+        int col0 = 1;
 
-        for (int i = 0; i < m; i++) {
-            if (matrix[i][0] == 0)
-                firstCol = true;
-        }
+        // Step 1: Mark rows and columns
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
 
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
+
+                    // Mark the row
                     matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+
+                    // Mark the column
+                    if (j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        col0 = 0;
                 }
             }
         }
 
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0)
-                    matrix[i][j] = 0;
+        // Step 2: Use the markers
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+
+                if (matrix[i][j] != 0) {
+
+                    if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
             }
         }
 
-        if (firstRow) {
-            for (int j = 0; j < n; j++)
+        // Step 3: Handle first row
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
                 matrix[0][j] = 0;
+            }
         }
 
-        if (firstCol) {
-            for (int i = 0; i < m; i++)
+        // Step 4: Handle first column
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
                 matrix[i][0] = 0;
+            }
         }
     }
 };
