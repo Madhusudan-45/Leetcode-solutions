9271e0bebb9a2370b2dae45ebd244889eb1da54d
Madhusudan-45 <madhusudan.451813@gmail.com>
2026-08-21 10:14:46 +0530

    Time: 0 ms (100%), Space: 7.9 MB (38.7%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -8,6 +8,31 @@ public:
         if (n == 1)
             return 1;
         
-        return fib(n - 1) + fib(n - 2);
+        int prev2 = 0;
+        int prev1 = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            
+            int current = prev1 + prev2;
+            
+            prev2 = prev1;
+            prev1 = current;
+        }
+        
+        return prev1;
     }
 };
+
+// class Solution {
+// public:
+//     int fib(int n) {
+        
+//         if (n == 0)
+//             return 0;
+        
+//         if (n == 1)
+//             return 1;
+        
+//         return fib(n - 1) + fib(n - 2);
+//     }
+// };
